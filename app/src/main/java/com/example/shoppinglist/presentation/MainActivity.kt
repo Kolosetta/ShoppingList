ef24738a482bc2e90cec8a501052992bba57ab03
Kolosetta <kolosovskiy91@gmail.com>
2022-06-15 23:50:43 +0300

    Шаблон под Clean Architecture

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.shoppinglist
+package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
