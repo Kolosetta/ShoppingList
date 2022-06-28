41fad96bc46e764f437e09a7ac24c4b7e9c74708
tr00per <kolosovskiy91@gmail.com>
2022-06-28 23:55:38 +0300

    Минимальная реализация Presentation слоя

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.DeleteShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopListUseCase
+
+class MainViewModel : ViewModel() {
+
+    //TODO пока реализация без инъекции зависимости, поэтому нарушается принцип Clean Arch
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+}
