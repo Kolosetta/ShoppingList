438af25212859ba309adad5b08f9f0db5048debd
tr00per <kolosovskiy91@gmail.com>
2022-07-28 22:36:44 +0300

    Добавлена База данных

---
+++ app/src/main/java/com/example/shoppinglist/data/AppDataBase.kt
@@ -0,0 +1,37 @@
+package com.example.shoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppDataBase : RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object{
+        private var INSTANCE: AppDataBase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "shop_item.db"
+
+        fun getInstance(application: Application) : AppDataBase{
+            INSTANCE?.let{
+                return it
+            }
+            //Реализация singleton Double Check
+            synchronized(LOCK){
+                INSTANCE?.let{
+                    return it
+                }
+                val db = Room.databaseBuilder(
+                    application,
+                    AppDataBase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = db
+                return db
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,24 @@
+package com.example.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModel>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItem(item: ShopItemDbModel)
+
+    @Query("DELETE FROM shop_items WHERE id=:itemId")
+    fun deleteShopItem(itemId: Int)
+
+    @Query("SELECT * FROM shop_items WHERE id=:itemId")
+    fun getShopItem(itemId: Int): ShopItemDbModel
+
+}
